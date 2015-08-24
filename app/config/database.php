210c2419315a0ffdf2d1f0924b05a23a0c7338c2
Windofelm <recep.hidir@gmail.com>
2015-08-24 09:17:24 +0300

    ForumController

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'laravelforum',
 			'username'  => 'root',
-			'password'  => 'Server2005!',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
